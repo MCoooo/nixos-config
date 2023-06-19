605f400218462966e97e344ab7825e909611a6b7
mcoooo <martin.coomber@hotmail.com>
2023-06-19 19:50:33 +0100

    add some wimpy

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -3,7 +3,6 @@
 {
   imports = [
     ./apps/zsh.nix
-    ./apps/micro.nix
   ];
   home.username = "dave";
   home.homeDirectory = "/home/dave";
@@ -21,5 +20,105 @@
     (nerdfonts.override { fonts = [ "FiraCode" ]; })
     tmux
   ];
+
+
+  programs = {
+    bat = {
+      enable = true;
+      extraPackages = with pkgs.bat-extras; [
+        batwatch
+        prettybat
+      ];
+    };
+    bottom = {
+      enable = true;
+      settings = {
+        colors = {
+          high_battery_color = "green";
+          medium_battery_color = "yellow";
+          low_battery_color = "red";
+        };
+        disk_filter = {
+          is_list_ignored = true;
+          list = ["/dev/loop"];
+          regex = true;
+          case_sensitive = false;
+          whole_word = false;
+        };
+        flags = {
+          dot_marker = false;
+          enable_gpu_memory = true;
+          group_processes = true;
+          hide_table_gap = true;
+          mem_as_value = true;
+          tree = true;
+        };
+      };
+    };
+    command-not-found.enable = true;
+    dircolors = {
+      enable = true;
+      enableBashIntegration = true;
+      enableFishIntegration = true;
+    };
+    direnv = {
+      enable = true;
+      enableBashIntegration = true;
+      nix-direnv = {
+        enable = true;
+      };
+    };
+    exa = {
+      enable = true;
+      enableAliases = true;
+      icons = true;
+    };
+    git = {
+      enable = true;
+      package = pkgs.gitFull;
+      config.credential.helper = "libsecret";
+    };
+    gpg.enable = true;
+    home-manager.enable = true;
+    info.enable = true;
+    jq.enable = true;
+    micro = {
+      enable = true;
+      settings = {
+        colorcolumn = 80;
+        colorscheme = "simple";
+        diffgutter = true;
+        rmtrailingws = true;
+        savecursor = true;
+        saveundo = true;
+        scrollbar = true;
+      };
+    };
+    powerline-go = {
+      enable = true;
+      settings = {
+        cwd-max-depth = 5;
+        cwd-max-dir-size = 12;
+        max-width = 60;
+      };
+    };
+    zoxide = {
+      enable = true;
+      enableBashIntegration = true;
+      enableFishIntegration = true;
+    };
+  };
+
+  services = {
+    gpg-agent = {
+      enable = true;
+      enableSshSupport = true;
+      pinentryFlavor = "curses";
+    };
+  };
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
   home.stateVersion = "22.11";
 }
